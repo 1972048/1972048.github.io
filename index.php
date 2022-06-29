3b43ec7e6783f4bc5cc19a824c66607b823f211d
1972048 <1972048@maranatha.ac.id>
2022-06-29 16:19:56 +0700

    -

--- index.php
+++ index.php
@@ -5,16 +5,19 @@
 </head>
 <body>
     <center>
-    <a href="indexadmin.php" class="blocks a">
+    <a class="blocks a" onclick="myFunction('indexadmin.php')">
         <img src="https://berbagitips.co/wp-content/uploads/2019/08/data-management-665x435.jpg">
         <h1>Data Management</h1>
     </a>
-    <a href="indexuser.php" class="blocks a">
+    <a class="blocks a" onclick="myFunction('indexuser.php')">
         <img src="https://www.ruparupa.com/blog/wp-content/uploads/2020/12/rekomendasi-cctv.jpg">
         <h1>CCTV</h1>
     </a>
     </center>
 </body>
 <script>
+    function myFunction(win){
+        window.location(win)
+    }
 </script>
 </html>
